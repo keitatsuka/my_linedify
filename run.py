7ed20a088e5a777371522ad2981be3f4dcfadc1d
keitatsuka <keita.tsuka@gmail.com>
2024-11-04 22:05:53 +0900

    run.pyで環境変数の取得をos.getenv()で実装

--- run.py
+++ run.py
@@ -1,14 +1,22 @@
+import os  # 追加
 from contextlib import asynccontextmanager
 from fastapi import FastAPI, Request, BackgroundTasks
 from linedify import LineDify
 
+# 環境変数からトークンやシークレットを取得
+line_channel_access_token = os.getenv("LINE_CHANNEL_ACCESS_TOKEN")
+line_channel_secret = os.getenv("LINE_CHANNEL_SECRET")
+dify_api_key = os.getenv("DIFY_API_KEY")
+dify_base_url = os.getenv("DIFY_BASE_URL")
+dify_user = os.getenv("DIFY_USER")
+
 # LINE Bot - Dify Agent Integrator
 line_dify = LineDify(
-    line_channel_access_token="YOUR_CHANNEL_ACCESS_TOKEN",
-    line_channel_secret="YOUR_CHANNEL_SECRET",
-    dify_api_key="DIFY_API_KEY",
-    dify_base_url="DIFY_BASE_URL",    # e.g. http://localhost/v1
-    dify_user="DIFY_USER"
+    line_channel_access_token=line_channel_access_token,
+    line_channel_secret=line_channel_secret,
+    dify_api_key=dify_api_key,
+    dify_base_url=dify_base_url,    # e.g. http://localhost/v1
+    dify_user=dify_user
 )
 
 # FastAPI
